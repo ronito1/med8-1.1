0e935c80904f58839dbefe3d48949a51ac9d92cd
Ronit Pathak <ronitpathak12345@gmail.com>
2025-06-04 19:11:07 +0530

    vdos

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,6 +140,32 @@
             // other options
         });
 
+        $(".popup-video").magnificPopup({
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false,
+            iframe: {
+                patterns: {
+                    youtube: {
+                        index: 'youtube.com/',
+                        id: 'v=',
+                        src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+                    },
+                    vimeo: {
+                        index: 'vimeo.com/',
+                        id: '/',
+                        src: 'https://player.vimeo.com/video/%id%?autoplay=1'
+                    },
+                    gmaps: {
+                        index: '//maps.google.',
+                        src: '%id%&output=embed'
+                    }
+                }
+            }
+        });
+
         $(".popup-gallery").magnificPopup({
             type: 'image',
             gallery: {
